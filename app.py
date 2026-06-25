d5a1a709bfcd7fdf961eccf4cd6e5a1a13a019c1
kamillyd711-coder <kamillyd711@gmail.com>
2026-06-25 15:16:37 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+from datetime import datetime
+
+import streamlit as st
+
+ARQUIVO_MENSAGENS = Path("mensagens.json")
+
+def carregar_mensagens():
+    print()
+
+def salvar_mensagens(mensagem):
+    print()
+
+def adicionar_mensagem(username,mensagem):
+    print()
+
+st.title("💬 Chat entre usuários")
